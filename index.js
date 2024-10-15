db14d7b36ad25c6795df410e744a2760625cfa13
atharsh07 <atharsh0425@gmail.com>
2024-10-15 19:37:08 +0530

    jwt practice

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const app = express()
-const crypto = require('crypto')
+const JWT_SECRET = "ramdomharkiratilovekiara";
 app.use(express.json())
 
-
 const users = []
-
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
 
 app.post('/signup', (req, res) => {
     const username = req.body.username
@@ -25,40 +13,38 @@ app.post('/signup', (req, res) => {
         username: username,
         password: password
     })
-    res.json({message: "ur signed in"})
-    console.log(users)
+    res.send({message: "your signed in"})
 })
-
 
 app.post("/signin", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const user = users.find((u) => u.username === username && u.password === password);
+    const user = users.find(user => user.username === username && user.password === password);
     if (user) {
-        const token = generateToken();
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
         user.token = token;
         res.send({
             token
         })
+        console.log(users);
     } else {
         res.status(403).send({
             message: "Invalid username or password"
         })
     }
-    console.log(users)
 });
 
-app.get("/me", (req, res) => {
-    const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
-    if (user) {
-        res.send({
-            username: user.username
-        })
-    } else {
-        res.status(401).send({
-            message: "Unauthorized"
-        })
+app.get('/me', (req, res) => {
+    const token = req.headers.token
+    const userinfodecoded = jwt.verify(token, JWT_SECRET)
+    const username = userinfodecoded.username
+    const user = users.find((user) => username == user.username)
+    if(user){
+        res.send({message: user.username})
+    }else{
+        res.send({mesage: 'invalid token or auth'})
     }
 })
 
